fdac388d04e14e53ec2a3d3d23cca4afefdfc81f
deon <dchung2422@gmail.com>
2022-10-25 15:52:28 +0800

    no message

--- Backend/routers/recordRouter.js
+++ Backend/routers/recordRouter.js
@@ -5,7 +5,7 @@ const pool = require('../db');
 
 router.get("/records", async (req, res) => {
   try {
-    const records = await pool.query("select tokenid, time from tracingrecords");
+    const records = await pool.query("select name, contact_no, email from users");
 
     return res.status(200).json(records.rows);
   } catch (error) {
