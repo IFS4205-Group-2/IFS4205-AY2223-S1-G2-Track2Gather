859e807ee5e7f343c7a4c092a040123f11d710fe
deon <dchung2422@gmail.com>
2022-10-26 13:48:13 +0800

    fix tracing records

--- Backend/routers/recordRouter.js
+++ Backend/routers/recordRouter.js
@@ -7,7 +7,7 @@ router.get("/records", async (req, res) => {
   try {
     const records = await pool.query("select name, contact_no, email from users");
 
-    return res.status(200).json(records.rows);
+    return res.json(records.rows);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal Server error' });
