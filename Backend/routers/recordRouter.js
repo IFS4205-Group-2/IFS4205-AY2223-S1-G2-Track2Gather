850edc3241733f7ea910c0d7cdb5efa1a01aa101
deon <dchung2422@gmail.com>
2022-10-26 23:21:32 +0800

    no message

--- Backend/routers/recordRouter.js
+++ Backend/routers/recordRouter.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 
+router.get("/contacts", async (req, res) => {
+  try {
+    
+    const contacts = await pool.query("SELECT name, contact_no, email, gender, zipcode, tid FROM users;");
+    console.log("contacts request received");
+    return res.status(200).json(contacts.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal Server error' });
+  }
+});
 
 router.get("/records", async (req, res) => {
   try {
@@ -15,16 +26,6 @@ router.get("/records", async (req, res) => {
   }
 });
 
-router.get("/contact", async (req, res) => {
-  try {
-    
-    const contact = await pool.query("SELECT name, contact_no, email, gender, zipcode, tid FROM users;");
-    console.log("records request received");
-    return res.status(200).json(contact.rows);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: 'Internal Server error' });
-  }
-});
+
 
 module.exports = router;
