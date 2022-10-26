a5ff5ae0a16041e49e8e614ecd41d885bd275df0
deon <dchung2422@gmail.com>
2022-10-27 01:01:19 +0800

    config db2

--- Backend/db2.js
+++ Backend/db2.js
@@ -1,17 +1,17 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = process.env.DATABASE_URL
+const pool = process.env.DATABASE2_URL
   ? new Pool({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE2_URL,
       ssl: { rejectUnauthorized: false },
     })
   : new Pool({
-      database: process.env.DATABASE_NAME,
-      host: process.env.DATABASE_HOST,
-      password: process.env.DATABASE_PASSWORD,
-      user: process.env.DATABASE_USER,
-      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE2_NAME,
+      host: process.env.DATABASE2_HOST,
+      password: process.env.DATABASE2_PASSWORD,
+      user: process.env.DATABASE2_USER,
+      port: process.env.DATABASE2_PORT,
     });
 
 module.exports = pool;
