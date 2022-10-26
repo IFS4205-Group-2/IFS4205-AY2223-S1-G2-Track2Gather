b878e03b49c5328bc600bb14cf6a17a43ff81d41
deon <dchung2422@gmail.com>
2022-10-26 21:52:39 +0800

    troubleshoot tracing records

--- Backend/routers/recordRouter.js
+++ Backend/routers/recordRouter.js
@@ -6,11 +6,12 @@ const pool = require('../db');
 router.get("/records", async (req, res) => {
   try {
     const records = await pool.query("select name, contact_no, email from users");
-
-    return res.json(records.rows);
+    console.log("records request received")
+    return res.status(200).json(records.rows);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal Server error' });
   }
 });
+
 module.exports = router;
