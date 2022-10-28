14f0f499a344609441a35da1168c1268334bed3b
deon <dchung2422@gmail.com>
2022-10-28 10:11:37 +0800

    no message

--- Frontend/src/components/AccountContext.jsx
+++ Frontend/src/components/AccountContext.jsx
@@ -11,7 +11,7 @@ const UserContext = ({ children }) => {
   });
   const navigate = useNavigate();
   useEffect(() => {
-    fetch("http://172.25.76.159:4000/auth/login", {
+    fetch("http://172.25.76.158:4000/auth/login", {
       credentials: "include",
       headers: {
         authorization: `Bearer ${user.token}`,
