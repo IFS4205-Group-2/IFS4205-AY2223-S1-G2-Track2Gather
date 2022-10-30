5c61985bc7ee5b94aaddfb3a630b335cd9900402
deon <dchung2422@gmail.com>
2022-10-30 19:57:57 +0800

    update backend contact tracer and researcher

--- Backend/controllers/statsController.js
+++ Backend/controllers/statsController.js
@@ -1,7 +1,9 @@
 const handleGetHealthAuthorityStats = require("./express/handleGetHealthAuthorityStats");
 const handleGetUserStats = require("./express/handleGetUserStats");
+const handleGetContactTracerStats = require("./express/handleGetContactTracerStats");
 
 module.exports = {
   handleGetHealthAuthorityStats,
   handleGetUserStats,
+  handleGetContactTracerStats,
 };
--- Backend/routers/researchRouter.js
+++ Backend/routers/researchRouter.js
@@ -4,8 +4,8 @@ const pool = require('../db');
 
 router.get("/research", async (req, res) => {
   try {
-    
-    const research = await pool.query("SELECT healthdata.vaccination vaccination, userdata.user_gender gender, userdata.year_of_birth yearofbirth, userdata.postal_code postal from get_all_health_data_researcher() AS healthdata LEFT JOIN get_all_user_data_researcher() AS userdata ON userdata.id = healthdata.id;");
+
+    const research = await pool.query("SELECT healthdata.vaccination vaccination, userdata.user_gender gender, userdata.year_of_birth yearofbirth, userdata.postal_code postal from get_all_user_data_researcher() AS userdata LEFT JOIN get_all_health_data_researcher() AS healthdata ON userdata.id = healthdata.id;");
    
     console.log("research request received");
     return res.status(200).json(research.rows);
