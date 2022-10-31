55e97a8e35b22939521e85999b6444d114d80ccb
deon <dchung2422@gmail.com>
2022-10-31 13:28:10 +0800

    researcher data

--- Backend/routers/researchRouter.js
+++ Backend/routers/researchRouter.js
@@ -5,7 +5,7 @@ const pool = require('../db');
 router.get("/research", async (req, res) => {
   try {
 
-    const research = await pool.query("SELECT healthdata.vaccination vaccination, userdata.user_gender gender, userdata.year_of_birth yearofbirth, userdata.postal_code postal from get_all_user_data_researcher() AS userdata LEFT JOIN get_all_health_data_researcher() AS healthdata ON userdata.id = healthdata.id;");
+    const research = await pool.query("SELECT healthdata.vaccination vaccination, healthdata.recent_test_result testresult, userdata.user_gender gender, userdata.year_of_birth yearofbirth, userdata.postal_code postal from get_all_user_data_researcher() AS userdata LEFT JOIN get_all_health_data_researcher() AS healthdata ON userdata.id = healthdata.id;");
    
     console.log("research request received");
     return res.status(200).json(research.rows);
