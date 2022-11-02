0b71cade6212627ebc78cbd1c99492150854176b
deon <dchung2422@gmail.com>
2022-11-03 00:29:18 +0800

    role validation for contact tracer

--- Backend/routers/recordRouter.js
+++ Backend/routers/recordRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const {
   handleGetTracingRecords,
 } = require("../controllers/recordsController");
+const validateContactTracerRole = require("../controllers/validateContactTracerRole");
 
-router.get('/records', handleGetTracingRecords);
+router.get('/records', validateContactTracerRole, handleGetTracingRecords);
 
 module.exports = router;
