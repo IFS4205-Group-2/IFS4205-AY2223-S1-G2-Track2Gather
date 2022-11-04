d520a2ee47676126229747d78f5b36dd1b91e3d7
GlendonNotGlen <glendonng38@gmail.com>
2022-11-05 01:14:13 +0800

    Alternate fix

--- Backend/controllers/express/attemptLogin.js
+++ Backend/controllers/express/attemptLogin.js
@@ -14,34 +14,43 @@ const attemptLogin = async (req, res) => {
       "SELECT c.uid, c.username, c.password_hash, u.rid FROM credentials c, users u WHERE c.uid = u.uid AND c.username = $1;",
       [req.body.username]
     );
-    const isSamePass = await bcrypt.compare(
-      req.body.password,
-      potentialLogin.rows[0].password_hash
-    );
-
-    if (potentialLogin.rowCount > 0 && isSamePass) {
-      const ip = req.headers["x-forwarded-for"];
-      logtail.info("User " + req.body.username + " in via login page.", {
-        ipaddress: ip
-      });
-
-      jwtSign(
-        {
-          username: req.body.username,
-          userid: potentialLogin.rows[0].uid,
-          roleid: potentialLogin.rows[0].rid,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "20mins" }
-      )
-        .then(token => {
-          res.json({ loggedIn: true, token });
-        })
-        .catch(err => {
-          console.log(err);
-          res.json({ loggedIn: false, status: "Try again later" });
-        }); 
+   
+    if (potentialLogin.rowCount > 0) {
+      const isSamePass = await bcrypt.compare(
+        req.body.password,
+        potentialLogin.rows[0].password_hash
+      );
+      if (isSamePass) {
+        const ip = req.headers["x-forwarded-for"];
+        logtail.info("User " + req.body.username + " in via login page.", {
+          ipaddress: ip
+        });
+  
+        jwtSign(
+          {
+            username: req.body.username,
+            userid: potentialLogin.rows[0].uid,
+            roleid: potentialLogin.rows[0].rid,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: "20mins" }
+        )
+          .then(token => {
+            res.json({ loggedIn: true, token });
+          })
+          .catch(err => {
+            console.log(err);
+            res.json({ loggedIn: false, status: "Try again later" });
+          }); 
+      } else {
+        res.json({ loggedIn: false, status: "Wrong username or password!" });
+      }
     } else {
+      //Run dummy bcrypt command
+      await bcrypt.compare(
+        req.body.password,
+        $2b$10$jrUVh8rzQ0Hx99XZU/MdKO9U6X/CHUHeebCS82YiAFAqVKoEylSoO
+      );      
       res.json({ loggedIn: false, status: "Wrong username or password!" });
     }
   }
